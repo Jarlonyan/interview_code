8ea9c4e19d3d15c29a3a1a77cc28c2539925be50
Jarlonyan <286568450@qq.com>
2019-02-22 10:13:29 +0800

    update

--- sort_algorithms/bubble_sort.cpp
+++ sort_algorithms/bubble_sort.cpp
@@ -15,10 +15,10 @@ void print_vec(vector<int>& a) {
 //挨个遍历元素，若j与j+1逆序，则交换。一遍之后，最大的元素会冒在最右端
 void bubble_sort(vector<int>& a) {
     int n = a.size();
-    for(int i=0;i<n-1;++i) {
-        for(int j=n-1; j>i; --j) {
-            if(a[j] < a[j-1])
-                swap(a[j], a[j-1]);
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < n-1; ++j) {
+            if(a[j+1] < a[j])
+                swap(a[j+1], a[j]);
         }
         print_vec(a);
     }
