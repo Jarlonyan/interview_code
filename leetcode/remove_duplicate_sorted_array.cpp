29310519a77e3b536ced7be8329a05ef1e1625cc
Jarlonyan <286568450@qq.com>
2019-03-07 11:18:54 +0800

    update

--- leetcode/remove_duplicate_sorted_array.cpp
+++ leetcode/remove_duplicate_sorted_array.cpp
@@ -17,28 +17,26 @@ void print_vec(vector<int>& a) {
     cout << endl;
 }
 
-int remove_duplicates(vector<int>& a) {
+/* 解法:设置两个指针,i快指针用于遍历,idx慢指针指向当前没有重复序列的最后一个元素
+        如果a[i]!=a[idx],那么idx右移一个位置(到了一个废弃位置)，然后a[idx]=a[i]
+        如果a[i]==a[idx],那么idx不动，i继续循环
+*/
+void remove_duplicates(vector<int>& a) {
     int n = a.size();
-    int idx=0; //idx记录最终解的最后一个元素
+    int idx=0;
     for(int i=1; i<n; ++i) {
-        if(a[i] == a[idx]) {
-            continue;
-        }
-        else {
+        if(a[i] != a[idx]) {
             idx++;
             a[idx] = a[i];
         }
     }
-    return idx;
+    a.resize(idx+1); //0~idx才是最终的解，其余的删除
 }
-
-
 
 int main() {
     vector<int> a = {1, 1, 1, 2, 5, 6, 6};
     print_vec(a);
-    int res = remove_duplicates(a);
-    cout<< res <<endl;
+    remove_duplicates(a);
     print_vec(a);
     return 0;
 }
