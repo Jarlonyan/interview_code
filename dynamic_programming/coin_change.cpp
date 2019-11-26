61cda29347557db0993ac724a7a996754b739bd9
Jarlonyan <286568450@qq.com>
2019-11-26 22:44:00 +0800

    update

---
+++ dynamic_programming/coin_change.cpp
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+/*
+假设有 1元,3元,5元的硬币若干(无限),现在需要凑出11元,问如何组合才能使硬币的数量最少?
+*/
+
+
+int dp_solve(vector<int>& a, int K, int n) {
+    return res;
+}
+
+int main() {
+    vector<int> a= {1,3,5};
+    int k = 11;
+
+    int ret = dp_solve(a, k);
+
+    cout<< ret<<endl;
+    return 0;
+}
+
+
