945c7bf617293aabde97389b53712d0f32def30c
Jarlonyan <286568450@qq.com>
2020-01-23 16:05:22 +0800

    update

--- advanced_data_structure/utils.h
+++ advanced_data_structure/utils.h
@@ -10,15 +10,33 @@ class Iterator{
   public:
     Iterator() : ptr(nullptr) {}
     explicit Iterator(T *p) : ptr(p) {};
-    Iterator &operator = (T *p) {
+    Iterator &operator=(T *p) {
         ptr = p;
         return *this;
     }
 
-    Iterator operation++(int) {
-        
+    Iterator &operator++(int) {
+        ptr = ptr->next[0];
+        return *this;
     }
 
+    Iterator operator++(int) {
+        Iterator temp = *this;
+        ptr = ptr->next[0];
+        return temp;
+    }
+
+    bool operator != (const Iterator &rhs) const { return ptr!=rhs.ptr;}
+    bool operator != (const T *p) const { return ptr!=p; }
+    bool operator == (const Iterator &rhs) const { return ptr==rhs.ptr; }
+    bool operator == (const T *p) const { return ptr==p; }
+    bool operator() const { return ptr!=nullptr; }
+    operator T *() const { return ptr; }
+    T *operator->() const { return ptr; }
+    T &operator*() const { return *ptr; }
+
+  private:
+    T *ptr;
 };
 
 
