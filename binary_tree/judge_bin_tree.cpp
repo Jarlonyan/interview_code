c65c996fc9755566f9dde3503e883cb2a6fe8eb0
Jarlonyan <286568450@qq.com>
2020-02-21 21:16:29 +0800

    update

--- binary_tree/judge_bin_tree.cpp
+++ binary_tree/judge_bin_tree.cpp
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
-#include <vector>
-#include <queue>
 #include <algorithm>
 using namespace std;
 
@@ -13,11 +11,16 @@ using namespace std;
 int judge_balance_bin_tree(TreeNode* T) {
     if (T==NULL) 
         return 0;
-    return 1 + get_node_count(T->left) + get_node_count(T->right);
+    int left = judge_balance_bin_tree(T->left);
+    int right = judge_balance_bin_tree(T->right);
+    if (left<0 || right<0 || abs(left-right)>1)
+        return -1;
+    else
+        return max(left, right) + 1;
 }
 
 // 判断二叉树是否对称
-int judge_symmetric_bin_tree(TreeNode* T, int k) {
+/*int judge_symmetric_bin_tree(TreeNode* T, int k) {
     if(T==NULL || k<=0) {
         return 0;
     }
@@ -25,13 +28,14 @@ int judge_symmetric_bin_tree(TreeNode* T, int k) {
         return 1;
     }
     return get_k_level_count(T->left, k-1) + get_k_level_count(T->right, k-1);
-}
+}*/
 
 int main() {
     string seq = "ABC##DE#G##F###";
     int pos = 0;
     TreeNode *T = create_bin_tree(seq, pos);
 
+    cout<< judge_balance_bin_tree(T)<<endl;
 
     destroy_bin_tree(T);
     return 0;
