e22bcf73bc0c68f24cd5312060aad20e7e0558de
Jarlonyan <286568450@qq.com>
2020-02-23 00:10:49 +0800

    update

---
+++ leetcode/3sum.cpp
@@ -0,0 +1,47 @@
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> three_sum(vector<int>& s) {
+    vector<vector<int>> ret;
+    int n = s.size();
+    int last = n-1;
+
+    sort(s.begin(), s.end());
+    for(int i=0; i<n-2; ++i) {
+        int j=i+1, k=last-1;
+        while(j<k) {
+            if(s[i]+s[j]+s[k] < 0) {
+                ++j;
+                while(s[j]==s[j-1] && j<k) ++j;
+            }
+            else if(s[i]+s[j]+s[k] < 0) {
+                --k;
+                while(s[k]==s[k+1] && j<k) --k;
+            }
+            else{
+                ret.push_back({s[i], s[j], s[k]});
+                ++j;
+                --k;
+                while(s[j]==s[j-1] && s[k]==s[k-1] && j<k) ++j;
+            }
+        }
+    }
+    return ret;
+}
+
+int  main() {
+    vector<int> a = {-1, 0, 1, 2, -1, -4};
+    auto ret = three_sum(a);
+    for(int i=0; i<ret.size(); ++i) {
+        for(int j=0;j<ret[i].size(); ++j) {
+            cout<< ret[i][j] <<" ";
+        } 
+        cout<<endl;
+    }
+    return 0;
+}
+
+
