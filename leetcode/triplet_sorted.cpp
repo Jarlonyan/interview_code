cdbb8a9c9c6e1cf4a0cc51e8d2f3c65abccf8398
Jarlonyan <286568450@qq.com>
2020-02-25 15:53:21 +0800

    update

---
+++ leetcode/triplet_sorted.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+//一个数组a，判断是否存在i<j<k，满足a[i]<a[j]<a[k]，头条面试题
+
+vector<vector<int>> three_sum(vector<int>& s) {
+    return ret;
+}
+
+int  main() {
+    vector<int> a = {6, 2, 99, 13, 56, 30};
+    return 0;
+}
+
+
